10ceedac4bf60036a08ceeab5d9556afc9ad9ca6
GSD-unity <shhsjk2002@gmail.com>
2023-01-26 19:10:07 +0900

    [test]

--- DevTest.cpp
+++ DevTest.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string.h>
 
-char name[] = "글로벌 변수";
+char name[] = " 글로벌 변수";
 
 class Product{
 	public:
@@ -12,8 +12,8 @@ class Product{
 
 Product::Product()
 {
-	char name[] = "로컬 변수";
-	strcpy(Product::name, "멤버 변수");
+	char name[] = " 로컬 변수";
+	strcpy(Product::name, " 멤버 변수");
 	
 	std::cout << "name =" << name << std::endl ;
 	std::cout << "name =" << Product::name << std::endl;
