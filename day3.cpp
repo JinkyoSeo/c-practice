711a1061e5e5236c715b3c4217608da9ed6bacd4
GSD-unity <shhsjk2002@gmail.com>
2023-01-30 19:10:39 +0900

    [day3] x,y point

--- day3.cpp
+++ day3.cpp
@@ -1,18 +1,54 @@
 #include <iostream>
-#include <cstring>
-class Person{
+
+class Point{
 	public:
-		int age;
-	    char name[10];
+		void Print();
+	    Point();
+	    void SetXY(int numX, int numY);
+	    void MoveLeft();
+	    void MoveRight();
+	private:
+		int x, y;
 };
+
+Point::Point(){
+	x = 50 ;
+	y = 50 ;
+	
+	Print();
+}
+
+void Point::SetXY(int numX, int numY){
+	x = numX ;
+	y = numY ;
+	
+}
+
+void Point::MoveLeft(){
+	x -= 1;
+	Print();
+}
+
+void Point::MoveRight(){
+	x += 1;
+	Print();
+}
+
+void Point::Print(){
+	std::cout << "x : " << x << ", y : " << y << std::endl;
+}
 
 int main()
 {
-	Person kim ; 
-	kim.age = 23 ;
-	strcpy(kim.name, "Sunghoon");
-	std::cout << "이름 : " << kim.name << std::endl;
-	std::cout << "나이 : " << kim.age << std::endl;
+	Point point1 ;
+	
+	point1.SetXY(30, 100);
+	point1.Print();
+	
+	point1.MoveLeft();
+	point1.MoveLeft();
+	point1.MoveRight();
+	point1.MoveLeft();
 	 
 	return 0;
 }
